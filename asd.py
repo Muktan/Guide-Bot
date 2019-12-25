50c90f38fbb2c750af6dd61442127334a928d024
Vyom <angerstick3@gmail.com>
2019-12-25 16:12:54 +0530

    Prototype with Saved Model

--- asd.py
+++ asd.py
@@ -22,68 +22,74 @@ try:
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
 except:
-	words = []
-	labels = []
-	docs_x = []
-	docs_y = []
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-	for intent in data["intents"]:
-		for pattern in intent["patterns"]:
-			wrds = nltk.word_tokenize(pattern)
-			words.extend(wrds)
-			docs_x.append(wrds)
-			docs_y.append(intent["tag"])
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-		if intent["tag"] not in labels:
-			labels.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-	words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-	words = sorted(list(set(words)))
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
-	labels = sorted(labels)
+    labels = sorted(labels)
 
-	training = []
-	output = []
+    training = []
+    output = []
 
-	out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-	for x, doc in enumerate(docs_x):
-		bag = []
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-		wrds = [stemmer.stem(w.lower()) for w in doc]
+        wrds = [stemmer.stem(w.lower()) for w in doc]
 
-		for w in words:
-			if w in wrds:
-				bag.append(1)
-			else:
-				bag.append(0)
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-		output_row = out_empty[:]
-		output_row[labels.index(docs_y[x])] = 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-		training.append(bag)
-		output.append(output_row)
+        training.append(bag)
+        output.append(output_row)
 
 
-	training = numpy.array(training)
-	output = numpy.array(output)
+    training = numpy.array(training)
+    output = numpy.array(output)
 
-	with open("data.pickle", "wb") as f:
-		pickle.dump((words, labels, training, output), f)
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
-tensorflow.reset_default_graph()
+    tensorflow.reset_default_graph()
 
-net = tflearn.input_data(shape=[None, len(training[0])])
-net = tflearn.fully_connected(net, 32)
-net = tflearn.fully_connected(net, 32)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-model = tflearn.DNN(net)
-model.load("model.tflearn")
-#model.fit(training,output,n_epoch=1000,batch_size=10,show_metric=True)
-#model.save("model.tflearn")
-	
-	
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 32)
+    net = tflearn.fully_connected(net, 32)
+    net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+    try:
+        model.load("model.tflearn")
+    except:
+        model.fit(training, output, n_epoch=1000, batch_size=10, show_metric=True)
+        model.save("model.tflearn")
+    
+
+
+
+
 def hello ():  
     r = sr.Recognizer()                                                                                   
     with sr.Microphone() as source:                                
@@ -132,6 +138,3 @@ def hello ():
 button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
 canvas1.create_window(150, 150, window=button1)
 root.mainloop()
-'''
-set PATH=%PATH%;C:\Program Files (x86)\sox-14-4-2
-'''
